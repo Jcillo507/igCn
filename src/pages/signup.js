fa622426e443ee3a4c2a9f56426d522cb979f3f9
Joe Cillo <JCillo507@yahoo.com>
2021-02-25 16:10:09 -0500

    signup working '

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,11 +1,61 @@
-import React, {useState , useEffect , useContext}from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react'
+import { Link, useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase'
 import * as ROUTES from '../constants/routes';
-
+import { doesUsernameExist } from '../services/firebase';
 import logo from '../images/logo.png'
 
-
 const SignUp = () => {
+  const history = useHistory()
+  const { firebase } = useContext(FirebaseContext)
+
+  const [username, setUsername] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = username === '' || fullName === '' || password === '' || emailAddress === '';
+
+
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+
+    const usernameExists = await doesUsernameExist(username);
+    if (!usernameExists.length) {
+      try {
+        const createdUserResult = await firebase.auth().createUserWithEmailAndPassword(emailAddress, password);
+
+        await createdUserResult.user.updateProfile({
+          displayName: username
+        });
+
+        await firebase.firestore().collection('users').add({
+          userId: createdUserResult.user.uid,
+          username: username.toLowerCase(),
+          fullName,
+          emailAddress: emailAddress.toLowerCase(),
+          following: [],
+          followers: [],
+          dateCreated: Date.now()
+        });
+
+        history.push(ROUTES.DASHBOARD)
+      } catch (error) {
+        setFullName('');
+        setError(error.message);
+      }
+    } else {
+      setUsername('');
+      setFullName('');
+      setEmailAddress('');
+      setPassword('');
+      setError('That username is already taken, please try another!')
+    }
+  }
+  useEffect(() => {
+    document.title = 'Sign Up - Instagram';
+  }, []);
   return (
     <div className="container flex mx-auto max-w-xs items-center h-screen">
       <div className="flex flex-col">
@@ -13,35 +63,45 @@ const SignUp = () => {
           <h1 className="flex justify-center w-full">
             <img src={logo} alt="Instagram" className="mt-2 w-6/12 mb-4" />
           </h1>
-
-          <form method="POST">
+          {error && <p className="mb-4 text-xs text-red-500">{error}</p>}
+          <form onSubmit={handleSignUp} method="POST">
             <input
               aria-label="Enter your username"
               className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2"
               type="text"
               placeholder="Username"
+              value={username}
+              onChange={({ target }) => setUsername(target.value.toLowerCase())}
             />
             <input
               aria-label="Enter your full name"
               className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2"
               type="text"
               placeholder="Full name"
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
             />
             <input
               aria-label="Enter your email address"
               className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2"
               type="text"
               placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value.toLowerCase())}
             />
             <input
               aria-label="Enter your password"
               className="text-sm text-gray w-full mr-3 py-5 px-4 h-2 border bg-gray-background rounded mb-2"
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
             />
             <button
+              disabled={isInvalid}
               type="submit"
-              className={`bg-blue-500 text-white w-full rounded h-8 font-bold`}
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${isInvalid && 'cursor-not-allowed opacity-50'
+                }`}
             >
               Sign Up
                         </button>
